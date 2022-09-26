8b0f5a34e6c53c2524e71bac173ca1d06a999b0f
alldaygzchen <gonzalochen66@gmail.com>
2022-09-26 23:24:21 +0800

    sliding window ongoing

--- slidingWindow/567_m_permutationInString.py
+++ slidingWindow/567_m_permutationInString.py
@@ -1,21 +1,16 @@
-
-# trick 
-# return s1_map ==s2_map
-
 class Solution:
 
     def getAndAdd(self,string,dict_map):
-        if string in dict_map:
-            dict_map[string]+=1
-        else:
-            dict_map[string]=1
+
+        dict_map[string]= dict_map.get(string,0)+1
+
 
     def getAndMinus(self,string,dict_map):
 
         dict_map[string]-=1
         
-        if dict_map[string]==0:
-            dict_map.pop(string)
+        # if dict_map[string]==0:
+        #     dict_map.pop(string)
 
 
     def checkInclusion(self, s1, s2):
@@ -34,33 +29,44 @@ class Solution:
             self.getAndAdd(s1[idx],s1_map)
             self.getAndAdd(s2[idx],s2_map)
 
-        if s1_map ==s2_map:
-                return True
+        
+
+        s1_cond = len(s1_map)
+        s2_cond = 0
+
+        for e in s1_map:
+            if e in s2_map and s1_map[e] == s2_map[e]:
+                s2_cond+=1
+
+
+
+        if s1_cond ==s2_cond:
+            return True
 
 
         while r < len(s2):
 
-            print(l,r)
-            print(s1_map,s2_map)
-
-            if s1_map ==s2_map:
-                return True
-
-
-
             self.getAndAdd(s2[r],s2_map)
-            self.getAndMinus(s2[l],s2_map)
+            
+            if s2[r] in s1_map and s2_map[s2[r]]==s1_map[s2[r]]:
+                s2_cond +=1
+            elif s2[r] in s1_map and s2_map[s2[r]]-1==s1_map[s2[r]]:
+                s2_cond -=1
+            
 
-            # if s1_map ==s2_map:
-            #     return True
+            self.getAndMinus(s2[l],s2_map)
+            
+            if s2[l] in s1_map and s2_map[s2[l]]==s1_map[s2[l]]:
+                s2_cond +=1
+            elif s2[l] in s1_map and s2_map[s2[l]]+1==s1_map[s2[l]]:
+                s2_cond -=1
+
+            
+
+            if s1_cond ==s2_cond:
+                return True
 
             l+=1
             r+=1
 
-        return s1_map ==s2_map
-            
-            
-
-s =Solution()
-print(s.checkInclusion("adc","dcda"))
-
+        return res      
