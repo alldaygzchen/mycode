5a69789070784c7f9e0dcef595a192668441d37c
alldaygzchen <gonzalochen66@gmail.com>
2022-11-08 23:53:46 +0800

    backtracking ongoin

--- backtracking/40_m_combinationSum2.py
+++ backtracking/40_m_combinationSum2.py
@@ -1,6 +1,82 @@
 #can use only one time and  arrays may contain same value
+# class Solution:
+#     def combinationSum2(self,candidates,target):
+
+#         self.res = []
+#         candidates.sort() # sorted (for duplicates when using while loop)
+#         self.candidates = candidates
+#         self.target =target
+#         self.helper([],0)
+
+#         return self.res
+
+
+#     def helper(self,cur,idx):
+        
+#         # print('cur',cur)
+        
+#         #edge case
+        
+#         if cur and sum(cur)>self.target :
+#             return
+        
+#         if cur and sum(cur)==self.target :
+#             self.res.append(cur[:])
+#             return
+
+#         if idx>=len(self.candidates):
+#             return
+
+#         cur.append(self.candidates[idx])
+#         # left side => at least contain one value of curr index
+#         self.helper(cur,idx+1)
+#         cur.pop()
+#         # right side => Do not contain the value of curr index
+#         while idx+1<len(self.candidates) and self.candidates[idx]==self.candidates[idx+1]:
+#             idx+=1
+#         self.helper(cur,idx+1)
+
 class Solution:
-    def combinationSum(self,candidates,target):
+    def combinationSum2(self,candidates,target):
+        candidates.sort()
+        self.res = []
+        self.candidates = candidates
+        self.helper([],0,target)
+        return self.res
+
+    def helper(self,cur,idx,target):
+        
+
+        # edge case: target == 0 
+        if target == 0:
+            self.res.append(cur[:])
+            return 
+        
+        # edge case: target<0
+        if target <0:
+            return
+
+        # vertical has no prev, while horizontal has
+        prev =-1 
+
+        
+        # for loop (make sure each parallel node is unequal[prevent duplicate result])
+        for i in range(idx,len(self.candidates)):
+
+            # edge case: pop(previous) == new value =>pass
+            if self.candidates[i] == prev:
+                continue
+
+            # append new value to curr
+            cur.append(self.candidates[i])
+            
+            # recursion helper function 
+            self.helper(cur,i+1,target-self.candidates[i])
+
+            # pop added new value
+            prev = cur.pop()
+            
 
 
-    def helper(self,idx,cur):
+
+
---
+++ backtracking/79_m_wordSearch.py
@@ -0,0 +1,36 @@
+# i think we are going to use preorder recursion
+
+# edge case: satisfies the word or length smaller than th word (prevent time execution)
+# every cell should have to be the starting point connected with four different directions 
+
+class Solution:
+    def exist(self, board, word):
+        self.res = None
+        self.word = word
+        self.board = board
+        self.rows = len(board)
+        self.cols = len(board[0])
+        self.path =set()
+
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.board[r][c] == self.word[0]:
+                    if self.helper(r, c, 0):
+                        return True
+        return False
+
+    def helper(self,r,c,idx):
+
+        if idx == len(self.word):
+            return True
+        if (r<0 or c<0 or r>=self.rows or c>=self.cols or self.word[idx]!= self.board[r][c] or (r,c) in self.path):
+            return False
+        self.path.add((r,c))
+        self.res = (
+            self.helper(r + 1, c, idx + 1)
+                or self.helper(r - 1, c, idx + 1)
+                or self.helper(r, c + 1, idx + 1)
+                or self.helper(r, c - 1, idx + 1)
+        )
+        self.path.remove((r,c))
+        return self.res 
