b75180ca25529f5849cf0aa7c6715355dfdc64d6
alldaygzchen <gonzalochen66@gmail.com>
2022-12-03 14:43:16 +0800

    practice ongoing

--- 1D-dynamicProgramming/764_minCostClimbingStairs_DP.py
+++ 1D-dynamicProgramming/764_minCostClimbingStairs_DP.py
@@ -1,24 +1,26 @@
 class Solution:
     """
-    dp = [] #max profit
-  
+    Cannot use greedy since there may be a step with higher cost
+    
+    
     """
-    def rob(self, nums):
+    def minCostClimbingStairs(self, cost):
+        
+        if len(cost)<=1:
+            return 0
 
-        if len(nums)==1:
-            return nums[0]
+        dp = [0]*(len(cost)+1)
+        dp[0]=0
+        dp[1]=0
 
-        if len(nums)==2:
-            return max(nums[0],nums[1])
 
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
-        dp[1] = nums[1]
+        for i in range(2,len(dp)):
+ 
+            dp[i] = min(dp[i-2]+cost[i-2],dp[i-1]+cost[i-1])
 
-        for i in range(2,len(nums)):
-            dp[i] = max(max(dp[0:i-1])+nums[i],dp[i-1])
 
-        return dp[len(nums)-1]
+        
+        return dp[len(dp)-1]
 
 
 s =Solution()
