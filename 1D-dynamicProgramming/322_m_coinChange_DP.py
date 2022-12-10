bc51d00ce969fe58ca0d86e3363fd6e9c3692063
alldaygzchen <gonzalochen66@gmail.com>
2022-12-10 15:10:05 +0800

    1D ongoing

--- 1D-dynamicProgramming/322_m_coinChange_DP.py
+++ 1D-dynamicProgramming/322_m_coinChange_DP.py
@@ -1,20 +1,20 @@
-# class Solution:
+class Solution:
 
-#     "dp = min coin to the objective"
+    "dp = min coin to the objective"
 
-#     def coinChange(self, coins, amount):
-#         dp  = [float('inf')]*(amount+1)
-#         dp[0] = 0
+    def coinChange(self, coins, amount):
+        dp  = [float('inf')]*(amount+1)
+        dp[0] = 0
         
-#         for a in range(1,amount+1):
-#                 for c in coins:
-#                     if a-c>=0:
-#                         dp[a] = min(dp[a], 1 + dp[a - c])
+        for a in range(1,amount+1):
+                for c in coins:
+                    if a-c>=0:
+                        dp[a] = min(dp[a], 1 + dp[a - c])
 
 
-#         if dp[amount] == float('inf'):
-#             return -1
-#         return dp[amount]
+        if dp[amount] == float('inf'):
+            return -1
+        return dp[amount]
 
 # class Solution:
 
@@ -45,33 +45,33 @@
 
 
 
-class Solution:
+# class Solution:
 
-    '''dp = min coin to the objective'''
+#     '''dp = min coin to the objective'''
 
-    def coinChange(self, coins, amount):
+#     def coinChange(self, coins, amount):
 
-        if amount==0:
-            return 0
+#         if amount==0:
+#             return 0
 
-        amounts = [ i+1 for i in range(amount)]
-        dp  = [float('inf')]*len(amounts)
+#         amounts = [ i+1 for i in range(amount)]
+#         dp  = [float('inf')]*len(amounts)
         
-        for idx,a in enumerate(amounts):
-            if a in coins:
-                dp[idx] = 1
+#         for idx,a in enumerate(amounts):
+#             if a in coins:
+#                 dp[idx] = 1
 
 
-        for i in range(len(amounts)):
-            if dp[i]!=1:
-                for c in coins:
-                    if amounts[i]-c>0:
-                        dp[i] = min(dp[i], 1 + dp[amounts[i]-c-1])
+#         for i in range(len(amounts)):
+#             if dp[i]!=1:
+#                 for c in coins:
+#                     if amounts[i]-c>0:
+#                         dp[i] = min(dp[i], 1 + dp[amounts[i]-c-1])
 
-        if dp[amount-1] == float('inf'):
-            return -1
+#         if dp[amount-1] == float('inf'):
+#             return -1
         
-        return dp[amount-1]
+#         return dp[amount-1]
 
 s = Solution()
 print(s.coinChange([1,2,5],11))
