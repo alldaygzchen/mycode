9cf6215fdd11bce1d9b24edf80f4395669de50db
alldaygzchen <gonzalochen66@gmail.com>
2022-12-21 23:59:46 +0800

    2d start

---
+++ 2D-dynamicProgramming/62_m_uniquePaths_postorder.py
@@ -0,0 +1,25 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int):
+        self.cache = [[-1]*n for i in range(m)]
+        self.rows = m
+        self.cols = n
+        return self.dfs_postorder(0,0)
+
+
+    def dfs_postorder(self,r,c):
+
+
+        if r>=self.rows or c>=self.cols:
+            return 0
+
+        if r==self.rows-1 and c==self.cols-1:
+            self.cache[r][c]=1
+            return self.cache[r][c]
+
+        if self.cache[r][c]!=-1:
+            return self.cache[r][c]
+
+        self.cache[r][c]=self.dfs_postorder(r+1,c) +self.dfs_postorder(r,c+1)
+        return self.cache[r][c]
+
+        
