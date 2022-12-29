e2b7f2cd4a4e63e45b932d316c83433a9de6168a
alldaygzchen <gonzalochen66@gmail.com>
2022-12-30 00:30:27 +0800

    2d ongoing

--- 2D-dynamicProgramming/312_h_burstBalloons/312_h_burstBallons_Postorder.py
+++ 2D-dynamicProgramming/312_h_burstBalloons/312_h_burstBallons_Postorder.py
@@ -1,19 +1,17 @@
 class Solution:
     def maxCoins(self, nums):
         self.nums = [1]+nums+[1]
-        self.cache ={}
+        self.cache = [[-1]*len(self.nums) for _ in range(len(self.nums))]
         return self.dfs_postorder(1,len(self.nums)-2)
 
     def dfs_postorder(self,l,r):
         if l>r:
             return 0
-        if (l,r) in self.cache:
-            return self.cache[(l,r)]
-
-        self.cache[(l,r)]= float("-inf")
+        if self.cache[l][r]!=-1:
+            return self.cache[l][r]
         
         for i in range(l,r+1):
             coin = self.nums[l-1] * self.nums[i] * self.nums[r+1]
             coin += self.dfs_postorder(l,i-1) + self.dfs_postorder(i+1,r)
-            self.cache[(l,r)]= max(self.cache[(l,r)],coin)
-        return self.cache[(l,r)] 
+            self.cache[l][r]= max(self.cache[l][r],coin)
+        return self.cache[l][r]
