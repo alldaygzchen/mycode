1512352de17d7854baf322b8617dfc02e16a6347
alldaygzchen <gonzalochen66@gmail.com>
2023-01-03 00:25:39 +0800

    greedy ongoing

--- greedy/53_m_maximumSubarray_kadane.py
+++ greedy/53_m_maximumSubarray_kadane.py
@@ -1,18 +1,13 @@
 class Solution:
     def maxSubArray(self, nums):
 
-        res = float('-inf')
-        total =0
+        maxSum = float('-inf')
+        curSum = 0
 
-        for num in nums:
-            
-            total+=num
-            res = max(res, total)
-           
-
-            if total<0:
-                total = 0 
-        return res
+        for n in nums:
+            curSum = max(curSum+n, n)
+            maxSum = max(maxSum, curSum)
+        return maxSum
 
     # def kadanes(nums):
     #     maxSum = nums[0]
@@ -23,3 +18,20 @@ class Solution:
     #         curSum += n
     #         maxSum = max(maxSum, curSum)
     #     return maxSum
+
+
+
+    # def maxSubArray(self, nums):
+
+    # res = float('-inf')
+    # total =0
+
+    # for num in nums:
+        
+    #     total+=num
+    #     res = max(res, total)
+        
+
+    #     if total<0:
+    #         total = 0 
+    # return res
